f3b1e90213012c3ae782bc346d4fbc7a26e50194
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-11-10 00:33:45 +0200

    refactor code

--- Pr2503/Sources/Controllers/ViewController.swift
+++ Pr2503/Sources/Controllers/ViewController.swift
@@ -1,9 +1,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var button: UIButton!
     
-    var isBlack: Bool = false {
+    // MARK: Outlets
+    
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+    
+    private var isBlack: Bool = false {
         didSet {
             if isBlack {
                 self.view.backgroundColor = .black
@@ -13,65 +16,47 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func onBut(_ sender: Any) {
-        isBlack.toggle()
-    }
+    // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
        // self.bruteForce(passwordToUnlock: "1!gr")
         
         // Do any additional setup after loading the view.
     }
     
-    func bruteForce(passwordToUnlock: String) {
-        let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
+    // MARK: Actions
+    
+    @IBAction func onChangeBackgroundColorPressed(_ sender: Any) {
+        isBlack.toggle()
+    }
+    
+    private func bruteForce(passwordToUnlock: String) {
+        let allowedCharacters: [String] = String().printable.map { String($0) }
 
         var password: String = ""
 
         // Will strangely ends at 0000 instead of ~~~
         while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
-            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+            password = generateBruteForce(password, fromArray: allowedCharacters)
 //             Your stuff here
             print(password)
             // Your stuff here
         }
-        
         print(password)
     }
 }
 
-
-
-extension String {
-    var digits:      String { return "0123456789" }
-    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
-    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
-    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
-    var letters:     String { return lowercase + uppercase }
-    var printable:   String { return digits + letters + punctuation }
-
-
-
-    mutating func replace(at index: Int, with character: Character) {
-        var stringArray = Array(self)
-        stringArray[index] = character
-        self = String(stringArray)
-    }
-}
-
-func indexOf(character: Character, _ array: [String]) -> Int {
+private func indexOf(character: Character, _ array: [String]) -> Int {
     return array.firstIndex(of: String(character))!
 }
 
-func characterAt(index: Int, _ array: [String]) -> Character {
+private func characterAt(index: Int, _ array: [String]) -> Character {
     return index < array.count ? Character(array[index])
                                : Character("")
 }
 
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+private func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
     var str: String = string
 
     if str.count <= 0 {
@@ -80,12 +65,10 @@ func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
     else {
         str.replace(at: str.count - 1,
                     with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
         if indexOf(character: str.last!, array) == 0 {
             str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
         }
     }
-
     return str
 }
 
